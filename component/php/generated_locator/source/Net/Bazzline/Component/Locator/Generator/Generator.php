31f67847b8d39ba0c86235c25d441b93276d554b
stevleibelt <stev@leibelt.de>
2014-04-24 17:51:38 +0200

    Added example code

---
+++ component/php/generated_locator/source/Net/Bazzline/Component/Locator/Generator/Generator.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author sleibelt
+ * @since 2014-04-24
+ */
+
+namespace Net\Bazzline\Component\Locator\Generator;
+
+/**
+ * Class Generator
+ *
+ * @package Net\Bazzline\Component\Locator
+ */
+class Generator
+{
+
+} 
