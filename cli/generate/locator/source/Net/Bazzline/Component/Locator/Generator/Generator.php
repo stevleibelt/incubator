3faac64c8efa6ff8f51b26117812e6a6423fb04b
stevleibelt <stev@leibelt.de>
2014-04-29 11:07:56 +0200

    sliced Generator class out and created generate script

--- cli/generate/locator/source/Net/Bazzline/Component/Locator/Generator/Generator.php
+++ cli/generate/locator/source/Net/Bazzline/Component/Locator/Generator/Generator.php
@@ -1,4 +1,3 @@
-#!/bin/php
 <?php
 /**
  * @author sleibelt
@@ -12,43 +11,6 @@ use Net\Bazzline\Component\Locator\Generator\Template\ClassTemplate;
 use Net\Bazzline\Component\Locator\Generator\Template\MethodTemplate;
 use Net\Bazzline\Component\Locator\Generator\Template\PhpDocumentationTemplate;
 use Net\Bazzline\Component\Locator\Generator\Template\PropertyTemplate;
-
-require_once __DIR__ . '/../../../../../../vendor/autoload.php';
-
-global $argc, $argv;
-
-$isNotCalledFromCommandLineInterface = (PHP_SAPI !== 'cli');
-
-$usageMessage = 'Usage: ' . PHP_EOL .
-    basename(__FILE__) . ' <path to configuration file> <output-path>' . PHP_EOL;
-
-if ($isNotCalledFromCommandLineInterface) {
-    echo 'This script can only be called from the command line' . PHP_EOL .
-        $usageMessage;
-    exit(1);
-}
-
-if ($argc !== 3) {
-    echo 'called with invalid number of arguments' . PHP_EOL;
-    echo $usageMessage;
-    exit(1);
-}
-
-$cwd = getcwd();
-$outputPath = $cwd . DIRECTORY_SEPARATOR . $argv[2];
-$pathToConfigurationFile = $cwd . DIRECTORY_SEPARATOR . $argv[1];
-
-try {
-    $generator = new Generator();
-    $generator->setOutputPath($outputPath);
-    $generator->setPathToConfigurationFile($pathToConfigurationFile);
-    $generator->generate();
-    echo 'locator written into "' . $outputPath . '"' . PHP_EOL;
-    exit(0);
-} catch (Exception $exception) {
-    echo $exception->getMessage() . PHP_EOL;
-    exit(1);
-}
 
 /**
  * Class Generator
@@ -140,6 +102,7 @@ array (
         $isInInstancePool->setName('isInInstancePool');
         $isInInstancePool->addParameter('key', '', 'string');
         $isInInstancePool->addParameter('type', '', 'string');
+        //replace with content block
         $isInInstancePool->setBody(array(
             'switch ($type) {',
             '    case \'factory\';',
---
+++ cli/generate/locator/source/Net/Bazzline/Component/Locator/Generator/generate.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author sleibelt
+ * @since 2014-04-29
+ */
+#!/bin/php
+
+use Net\Bazzline\Component\Locator\Generator\Generator;
+
+require_once __DIR__ . '/../../../../../../vendor/autoload.php';
+
+global $argc, $argv;
+
+$isNotCalledFromCommandLineInterface = (PHP_SAPI !== 'cli');
+
+$usageMessage = 'Usage: ' . PHP_EOL .
+    basename(__FILE__) . ' <path to configuration file> <output-path>' . PHP_EOL;
+
+if ($isNotCalledFromCommandLineInterface) {
+    echo 'This script can only be called from the command line' . PHP_EOL .
+        $usageMessage;
+    exit(1);
+}
+
+if ($argc !== 3) {
+    echo 'called with invalid number of arguments' . PHP_EOL;
+    echo $usageMessage;
+    exit(1);
+}
+
+$cwd = getcwd();
+$outputPath = $cwd . DIRECTORY_SEPARATOR . $argv[2];
+$pathToConfigurationFile = $cwd . DIRECTORY_SEPARATOR . $argv[1];
+
+try {
+    $generator = new Generator();
+    $generator->setOutputPath($outputPath);
+    $generator->setPathToConfigurationFile($pathToConfigurationFile);
+    $generator->generate();
+    echo 'locator written into "' . $outputPath . '"' . PHP_EOL;
+    exit(0);
+} catch (Exception $exception) {
+    echo $exception->getMessage() . PHP_EOL;
+    exit(1);
+}
