bdf6016f33e11b8dfcc5559c5858325f82e76b3b
stevleibelt <stev@leibelt.de>
2014-05-26 22:05:32 +0200

    Scribbled idea of FileExistsStrategy

---
+++ cli/generate/locator/source/Net/Bazzline/Component/Locator/FileExistsStrategy/RuntimeException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-05-26 
+ */
+
+namespace Net\Bazzline\Component\Locator\FileExistsStrategy;
+
+use RuntimeException as ParentClass;
+
+/**
+ * Class RuntimeException
+ * @package Net\Bazzline\Component\Locator\FileExistsStrategy
+ */
+class RuntimeException extends ParentClass {}
