11a5f0bec11595641dfa4bba8a9a8787779189c9
stevleibelt <stev@leibelt.de>
2014-06-09 23:42:12 +0200

    prepared configuration\assembler section for testing

---
+++ cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/Configuration/Assembler/FromArrayAssemblerTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-06-09 
+ */
+
+namespace Test\Net\Bazzline\Component\Locator\Configuration\Assembler;
+
+use Test\Net\Bazzline\Component\Locator\LocatorTestCase;
+
+/**
+ * Class FromArrayAssemblerTest
+ * @package Test\Net\Bazzline\Component\Locator\Configuration\Assembler
+ */
+class FromArrayAssemblerTest extends LocatorTestCase
+{
+    public function testWithInvalidData()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testWithValidData()
+    {
+        $this->markTestIncomplete();
+    }
+} 
---
+++ cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/Configuration/Assembler/FromPropelSchemaXmlAssemblerTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-06-09 
+ */
+
+namespace Test\Net\Bazzline\Component\Locator\Configuration\Assembler;
+
+use Test\Net\Bazzline\Component\Locator\LocatorTestCase;
+
+/**
+ * Class FromPropelSchemaXmlAssemblerTest
+ * @package Test\Net\Bazzline\Component\Locator\Configuration\Assembler
+ */
+class FromPropelSchemaXmlAssemblerTest extends LocatorTestCase
+{
+    public function testWithInvalidData()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testWithValidData()
+    {
+        $this->markTestIncomplete();
+    }
+}
