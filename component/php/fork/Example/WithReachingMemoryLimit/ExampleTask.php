25909c4709ade96f55f65d2d2546c1386ccbd48b
stevleibelt <stev@leibelt.de>
2014-07-23 23:00:37 +0200

    added example for reaching memory limit

---
+++ component/php/fork/Example/WithReachingMemoryLimit/ExampleTask.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-07-23 
+ */
+
+namespace Net\Bazzline\Component\Fork\Example\WithReachingMemoryLimit;
+
+use Net\Bazzline\Component\Fork\AbstractTask;
+
+/**
+ * Class ExampleTask
+ * @package Net\Bazzline\Component\Fork\Example\WithReachingMemoryLimit
+ */
+class ExampleTask extends AbstractTask
+{
+    /**
+     * @var int
+     */
+    private $numberOfDataMultiplication;
+
+    /**
+     * @return int
+     */
+    public function getNumberOfDataMultiplication()
+    {
+        return $this->numberOfDataMultiplication;
+    }
+
+    /**
+     * @param int $number
+     */
+    public function setNumberOfDataMultiplication($number)
+    {
+        $this->numberOfDataMultiplication = $number;
+    }
+
+    /**
+     * @throws \Net\Bazzline\Component\Fork\RuntimeException
+     */
+    public function execute()
+    {
+        $identifier = 'task (' . posix_getpid() . ' / ' . $this->getParentProcessId() . ')';
+        $startTime = time();
+        $workingDataSet = array();
+
+        echo $identifier . ' says hello' . PHP_EOL;
+        echo $identifier . ' number of data multiplication ' . $this->numberOfDataMultiplication . PHP_EOL;
+        echo $identifier . ' memory usage ' . (memory_get_usage(true) / 1024 / 1024) . PHP_EOL;
+        echo PHP_EOL;
+
+        for ($iterator = 0; $iterator < $this->numberOfDataMultiplication; ++$iterator) {
+            //begin creating one megabyte of data
+            //taken from https://github.com/stevleibelt/examples/blob/master/php/array/createOneMegaByteOfData.php
+            $anotherRound = true;
+            $data = array();
+            $initialMemoryUsage = memory_get_usage(true);
+            $anotherIterator = 0;
+            $memoryUsageInMegaByteToGenerate = 1;
+
+            while ($anotherRound) {
+                $data[] = $anotherIterator;
+                $currentMemoryUsage = (memory_get_usage(true) - $initialMemoryUsage);
+                $currentMemoryUsageInMegaBytes = ($currentMemoryUsage / (1024 * 1024));
+                if ($currentMemoryUsageInMegaBytes === $memoryUsageInMegaByteToGenerate) {
+                    $anotherRound = false;
+                }
+                ++$anotherIterator;
+            }
+            //end creating one megabyte of data
+            $workingDataSet[] = $data;
+        }
+
+        echo $identifier . ' says goodbye' . PHP_EOL;
+        echo $identifier . ' runtime: ' . (time() - $startTime) . ' seconds' . PHP_EOL;
+        echo $identifier . ' memory usage ' . (memory_get_usage(true) / 1024 / 1024) . PHP_EOL;
+        echo PHP_EOL;
+    }
+}
--- component/php/fork/Example/WithReachingMemoryLimit/run.php
+++ component/php/fork/Example/WithReachingMemoryLimit/run.php
@@ -4,7 +4,7 @@
  * @since 2014-07-20 
  */
 
-namespace Net\Bazzline\Component\Fork\Example\WithReachingRunTimeLimit;
+namespace Net\Bazzline\Component\Fork\Example\WithReachingMemoryLimit;
 
 require_once __DIR__ . '/../bootstrap.php';
 require_once 'ExampleTask.php';
@@ -12,15 +12,14 @@ require_once 'ExampleTask.php';
 use Net\Bazzline\Component\Fork\Manager;
 
 $manager = new Manager();
-$maximumRunTimeInSeconds = 9;
 
 $taskOne = new ExampleTask();
 $taskTwo = new ExampleTask();
 $taskThree = new ExampleTask();
 
-$taskOne->setRunTime(($maximumRunTimeInSeconds - 3)); //there is an offset of 2 seconds
-$taskTwo->setRunTime($maximumRunTimeInSeconds);
-$taskThree->setRunTime(($maximumRunTimeInSeconds + 1));
+$taskOne->setNumberOfDataMultiplication(3);
+$taskTwo->setNumberOfDataMultiplication(4);
+$taskThree->setNumberOfDataMultiplication(5);
 
 /**
  * @var array|ExampleTask[] $tasks
@@ -35,12 +34,13 @@ foreach ($tasks as $task) {
     $manager->addTask($task);
 }
 
-$manager->setMaximumSecondsOfRunTime($maximumRunTimeInSeconds);
+$manager->getMemoryLimitManager()->setBufferInMegaBytes(1);
+$manager->getMemoryLimitManager()->setMaximumInMegaBytes(4);
 $manager->execute();
 
 foreach ($tasks as $key => $task) {
     $status = ($task->isAborted()) ?
         'aborted' : (($task->isFinished()) ?
             'finished' : 'not finished');
-    echo 'task ' . $key . ' with run time of ' . $task->getRunTime() . ' seconds has been ' . $status . PHP_EOL;
+    echo 'task ' . $key . ' with run time of ' . $task->getNumberOfDataMultiplication() . ' seconds has been ' . $status . PHP_EOL;
 }
