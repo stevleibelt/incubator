cc0ff38ab8eabb092ce18dac6ab0e79b6edd7770
stevleibelt <stev@leibelt.de>
2014-08-07 23:37:33 +0200

    added tests

--- cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/Configuration/Assembler/FromArrayAssemblerTest.php
+++ cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/Configuration/Assembler/FromArrayAssemblerTest.php
@@ -14,12 +14,108 @@ use Test\Net\Bazzline\Component\Locator\LocatorTestCase;
  */
 class FromArrayAssemblerTest extends LocatorTestCase
 {
-    public function testWithInvalidData()
+    /**
+     * @expectedException \Net\Bazzline\Component\Locator\Configuration\Assembler\RuntimeException
+     * @expectedExceptionMessage configuration is mandatory
+     */
+    public function testAssembleMissingProperties()
     {
-$this->markTestIncomplete();
+        $assembler = $this->getFromArrayAssembler();
+
+        $assembler->assemble(null);
     }
 
-    public function testWithValidData()
+    /**
+     * @expectedException \Net\Bazzline\Component\Locator\Configuration\Assembler\InvalidArgumentException
+     * @expectedExceptionMessage data must be an array
+     */
+    public function testAssembleWithNoArrayAsData()
+    {
+        $assembler = $this->getFromArrayAssembler();
+
+        $configuration = $this->getMockOfConfiguration();
+        $assembler->setConfiguration($configuration);
+
+        $assembler->assemble(null);
+    }
+
+    /**
+     * @expectedException \Net\Bazzline\Component\Locator\Configuration\Assembler\InvalidArgumentException
+     * @expectedExceptionMessage data array must contain content
+     */
+    public function testAssembleWithEmptyDataArray()
+    {
+        $assembler = $this->getFromArrayAssembler();
+
+        $configuration = $this->getMockOfConfiguration();
+        $assembler->setConfiguration($configuration);
+
+        $assembler->assemble(array());
+    }
+
+    /**
+     * @expectedException \Net\Bazzline\Component\Locator\Configuration\Assembler\InvalidArgumentException
+     * @expectedExceptionMessage data array must contain content for key "class_name"
+     */
+    public function testAssembleWithMissingMandatoryDataKeyClassName()
+    {
+        $assembler = $this->getFromArrayAssembler();
+
+        $configuration = $this->getMockOfConfiguration();
+        $assembler->setConfiguration($configuration);
+
+        $assembler->assemble(array('key' => null));
+    }
+
+    /**
+     * @expectedException \Net\Bazzline\Component\Locator\Configuration\Assembler\InvalidArgumentException
+     * @expectedExceptionMessage value of key "class_name" must be of type "string"
+     */
+    public function testAssembleWithWrongMandatoryDataKeyClassNameValueType()
+    {
+        $assembler = $this->getFromArrayAssembler();
+
+        $configuration = $this->getMockOfConfiguration();
+        $assembler->setConfiguration($configuration);
+
+        $assembler->assemble(array('class_name' => 1));
+    }
+
+    /**
+     * @expectedException \Net\Bazzline\Component\Locator\Configuration\Assembler\InvalidArgumentException
+     * @expectedExceptionMessage data array must contain content for key "file_path"
+     */
+    public function testAssembleWithMissingMandatoryDataKeyFilePath()
+    {
+        $assembler = $this->getFromArrayAssembler();
+
+        $configuration = $this->getMockOfConfiguration();
+        $assembler->setConfiguration($configuration);
+
+        $assembler->assemble(array('class_name' => 'class name'));
+    }
+
+    /**
+     * @expectedException \Net\Bazzline\Component\Locator\Configuration\Assembler\InvalidArgumentException
+     * @expectedExceptionMessage value of key "extends" must be of type "array" when set
+     */
+    public function testAssembleWithWrongOptionalDataKeyClassNameValueType()
+    {
+        $assembler = $this->getFromArrayAssembler();
+
+        $configuration = $this->getMockOfConfiguration();
+        $assembler->setConfiguration($configuration);
+
+        $assembler->assemble(
+            array(
+                'class_name'    => 'class name',
+                'file_path'     => '/file/path',
+                'extends'       => 'your argument is invalid'
+            )
+        );
+    }
+
+    public function testAssembleWithValidData()
     {
 $this->markTestIncomplete();
     }
