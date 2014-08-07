ba87d0ad457021c11c94aeed9b13e60c9219312f
stevleibelt <stev@leibelt.de>
2014-08-07 23:11:49 +0200

    more tests to create

---
+++ cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/FactoryInterfaceGeneratorTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-08-07 
+ */
+
+namespace Test\Net\Bazzline\Component\Locator;
+
+/**
+ * Class FactoryInterfaceGeneratorTest
+ * @package Test\Net\Bazzline\Component\Locator
+ */
+class FactoryInterfaceGeneratorTest extends LocatorTestCase
+{
+    public function testSetters()
+    {
+$this->markTestIncomplete();
+    }
+
+    public function testGenerate()
+    {
+$this->markTestIncomplete();
+    }
+} 
---
+++ cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/InvalidArgumentExceptionGeneratorTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-08-07 
+ */
+
+namespace Test\Net\Bazzline\Component\Locator;
+
+/**
+ * Class InvalidArgumentExceptionGeneratorTest
+ * @package Test\Net\Bazzline\Component\Locator
+ */
+class InvalidArgumentExceptionGeneratorTest extends LocatorTestCase
+{
+    public function testSetters()
+    {
+$this->markTestIncomplete();
+    }
+
+    public function testGenerate()
+    {
+$this->markTestIncomplete();
+    }
+}
