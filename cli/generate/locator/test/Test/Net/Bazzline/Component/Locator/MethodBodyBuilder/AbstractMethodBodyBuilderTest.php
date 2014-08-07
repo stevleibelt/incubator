57d0379f81082e76fa10328d08cd4f6dbaadee5d
stevleibelt <stev@leibelt.de>
2014-08-07 23:02:54 +0200

    yep, more not done unittests

---
+++ cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/MethodBodyBuilder/AbstractMethodBodyBuilderTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-08-07 
+ */
+
+namespace Test\Net\Bazzline\Component\Locator\MethodBodyBuilder;
+
+use Test\Net\Bazzline\Component\Locator\LocatorTestCase;
+
+/**
+ * Class AbstractMethodBodyBuilderTest
+ * @package Test\Net\Bazzline\Component\Locator\MethodBodyBuilder
+ */
+class AbstractMethodBodyBuilderTest
+{
+
+} 
---
+++ cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/MethodBodyBuilder/PropelQueryCreateBuilderTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-08-07 
+ */
+
+namespace Test\Net\Bazzline\Component\Locator\MethodBodyBuilder;
+
+/**
+ * Class PropelQueryCreateBuilderTest
+ * @package Test\Net\Bazzline\Component\Locator\MethodBodyBuilder
+ */
+class PropelQueryCreateBuilderTest
+{
+
+} 
