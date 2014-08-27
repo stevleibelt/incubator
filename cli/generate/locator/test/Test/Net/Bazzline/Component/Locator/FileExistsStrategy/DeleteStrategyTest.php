f991d9aef8fbe64a619113898277ac76a5c75ce1
stevleibelt <stev@leibelt.de>
2014-08-27 23:38:35 +0200

    another unittest bites the dust

--- cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/FileExistsStrategy/DeleteStrategyTest.php
+++ cli/generate/locator/test/Test/Net/Bazzline/Component/Locator/FileExistsStrategy/DeleteStrategyTest.php
@@ -6,6 +6,7 @@
 
 namespace Test\Net\Bazzline\Component\Locator\FileExistsStrategy;
 
+use org\bovigo\vfs\vfsStream;
 use Test\Net\Bazzline\Component\Locator\LocatorTestCase;
 
 /**
@@ -16,11 +17,28 @@ class DeleteStrategyTest extends LocatorTestCase
 {
     public function testExecuteWithNotDeletableFile()
     {
-$this->markTestIncomplete();
+        $this->markTestSkipped('vfsStream seams to have a bug inside. We can unlink this file, even without having read or write permissions.');
+        $strategy = $this->getDeleteStrategy();
+        $root = vfsStream::setup('root', 0700);
+        $file = vfsStream::newFile('file', 0000);
+        $root->addChild($file);
+
+        $strategy->setFilePath($root->url());
+        $strategy->setFileName('file');
+
+        $strategy->execute();
     }
 
     public function testExecuteWithDeletableFile()
     {
-$this->markTestIncomplete();
+        $strategy = $this->getDeleteStrategy();
+        $root = vfsStream::setup('root', 0700);
+        $file = vfsStream::newFile('file', 0700);
+        $root->addChild($file);
+
+        $strategy->setFilePath($root->url());
+        $strategy->setFileName('file');
+
+        $strategy->execute();
     }
 }
