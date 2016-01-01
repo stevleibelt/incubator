7205e549df6a40377417d5c9ba9bd6f19ef083f2
stevleibelt <stev@leibelt.de>
2016-01-01 13:32:20 +0100

    fixed stupit issues

--- general/uberspace_lets_encrypt/cronjob.sh
+++ general/uberspace_lets_encrypt/cronjob.sh
@@ -7,9 +7,22 @@
 # @since: 2015-12-28
 ####
 
+#begin of local parameters
 LOCAL_ROOT_PATH='/home/<user name>'
 LOCAL_LOG_PATH=$LOCAL_ROOT_PATH'/<path to your log files>'
 LOCAL_ACCOUNT='<your.domain.tld>'
+#end of local parameters
 
-letsencrypt-renewer --config-dir $LOCAL_ROOT_PATH'/.config/letsencrypt --logs-dir '$LOCAL_ROOT_PATH'/.config/letsencrypt/logs --work-dir '$LOCAL_ROOT_PATH'/tmp/' 1&2>$LOCAL_LOG_PATH
-uberspace-prepare-certificate -k $LOCAL_ROOT_PATH'/.config/letsencrypt/live/'$LOCAL_ACCOUNT'/privkey.pem' -c $LOCAL_ROOT_PATH'/.config/letsencrypt/live/'$LOCAL_ACCOUNT'/cert.pem' 1&2>>$LOCAL_LOG_PATH
+#begin of parameters for letsencrypt-renewer
+LOCAL_CONFIGURATION_PATH=$LOCAL_ROOT_PATH'/.config/letsencrypt'
+LOCAL_LOGGING_PATH=$LOCAL_ROOT_PATH'/.config/letsencrypt/logs'
+LOCAL_WORING_PATH=$LOCAL_ROOT_PATH'/tmp/'
+#end of parameters for letsencrypt-renewer
+
+#begin of parameters for uperspace-prepare-certificate
+LOCAL_KEY_PATH=$LOCAL_ROOT_PATH'/.config/letsencrypt/live/'$LOCAL_ACCOUNT'/privkey.pem'
+LOCAL_CERTIFICATE_PATH=$LOCAL_ROOT_PATH'/.config/letsencrypt/live/'$LOCAL_ACCOUNT'/cert.pem'
+#end of parameters for uperspace-prepare-certificate
+
+letsencrypt-renewer --config-dir $LOCAL_CONFIGURATION_PATH --logs-dir $LOCAL_LOGGING_PATH --work-dir $LOCAL_WORING_PATH &>$LOCAL_LOG_PATH
+uberspace-prepare-certificate -k $LOCAL_KEY_PATH -c $LOCAL_CERTIFICATE_PATH &>>$LOCAL_LOG_PATH
