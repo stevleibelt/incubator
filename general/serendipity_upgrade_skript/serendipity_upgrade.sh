b8db0469f14a50496f4f25d300b0cd6155340f8f
stevleibelt <stev@leibelt.de>
2016-09-27 16:30:07 +0200

    Small fixes

--- general/serendipity_upgrade_skript/serendipity_upgrade.sh
+++ general/serendipity_upgrade_skript/serendipity_upgrade.sh
@@ -3,10 +3,12 @@
 # @author stev leibelt <artodeto@bazzline.net>
 # @since 2016-09-26
 # @see http://docs.s9y.org/docs/faq/index.html
-# @todo ask to make a database backup
+# @todo
+#   ask to make a database backup
+#   move stuff into functions pre_deploy, deploy and post_deploy
 ####
 
-CURRENT_DATE_AS_STRING=$(date '%Y-%m-%d')
+CURRENT_DATE_AS_STRING=$(date +'%Y-%m-%d')
 CURRENT_INSTALLED_VERSION_SH512_SUM=current_installation.sha512
 CURRENT_WORKING_DIRECTORY=$(pwd)
 DIRECTORY_NAME_OF_NEW_VERSION="serendipity"
@@ -41,7 +43,7 @@ fi
 PATH_TO_THE_SERENDIPITY_INSTALLATION="$1"
 PATH_TO_SWITCH_TO=$(dirname ${PATH_TO_THE_SERENDIPITY_INSTALLATION})
 CONFIGURATION_FILE_NAME="serendipity_config_local.inc.php"
-RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION=$(basename ${$PATH_TO_THE_SERENDIPITY_INSTALLATION})
+RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION=$(basename ${PATH_TO_THE_SERENDIPITY_INSTALLATION})
 RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION_BACKUP="${RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION}.${CURRENT_DATE_AS_STRING}"
 
 if [[ ! -d "${PATH_TO_THE_SERENDIPITY_INSTALLATION}" ]];
@@ -65,7 +67,8 @@ fi
 cd ${PATH_TO_SWITCH_TO}
 
 #download from http://www.s9y.org/latest
-wget http://www.s9y.org/latest
+echo ":: Downloading latest version."
+wget --quet http://www.s9y.org/latest
 #rename latest to latest.zip
 mv latest ${FILE_NAME_OF_NEW_VERSION}
 
@@ -75,7 +78,7 @@ then
     #sha256sum latest.zip
     if sha512sum -c --quiet --status ${CURRENT_INSTALLED_VERSION_SH512_SUM};
     then
-        echo ":: Newest release already installed"
+        echo ":: Newest version already installed."
         tear_down
         exit 0
     else
@@ -93,9 +96,11 @@ sha512sum ${FILE_NAME_OF_NEW_VERSION} > ${CURRENT_INSTALLED_VERSION_SH512_SUM}
 mv ${RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION} ${RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION_BACKUP}
 
 #   unzip latest.zip
-
-unzip ${FILE_NAME_OF_NEW_VERSION}
+echo ":: Decompressing latest version."
+unzip -qq ${FILE_NAME_OF_NEW_VERSION}
 chmod -R 755 ${DIRECTORY_NAME_OF_NEW_VERSION}
+rm -fr ${FILE_NAME_OF_NEW_VERSION}
+
 #   mv latest $public
 mv ${DIRECTORY_NAME_OF_NEW_VERSION} ${RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION}
 
@@ -115,7 +120,10 @@ then
     if [[ ${YES_OR_NO} != "n" ]];
     then
         tar -zcf ${RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION_BACKUP}.tar.gz ${RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION_BACKUP}
+        rm -fr ${RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION_BACKUP}
     fi
 else
     rm -fr ${RELATIVE_PATH_TO_THE_SERENDIPITY_INSTALLATION_BACKUP}
 fi
+
+echo ":: Done"
